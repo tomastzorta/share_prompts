0ae2d50e83a0ce57d6989a7a9053b4ed228e9989
University Account <greekharvarddev@gmail.com>
2023-09-09 04:53:10 +0300

    Setting up

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import '@styles/globals.css';
+
+import Nav from '@components/Nav'; //declaring it here cause layout.jsx is consistent throughout all pages
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "Promptopia",
+    description: 'Discover & Share AI prompts'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className="main">
+                <div className="gradient" />
+            </div>
+
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout;
